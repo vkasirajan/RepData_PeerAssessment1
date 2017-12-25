ec03e40b5d3f3f6654d466fd957c00b289cd96bf
venkatk <venkateswaran_kasirajan@trimble.com>
2017-12-24 21:57:07 -0700

    Fourth Step

--- Project1.R
+++ Project1.R
@@ -1,10 +1,10 @@
+# Step 1
+# Loading and preprocessing the data
 # Clean up a little bit and set working directory.
 # Load necessary libraries
 rm(list = ls())
 setwd("C:/Data Science/05 - Reproducible Research/project1/RepData_PeerAssessment1/")
 library(ggplot2)
-# Step 1
-# Loading and preprocessing the data
 
 # Check and unzip the file
 if (!file.exists("activity.csv")) {
@@ -71,9 +71,34 @@ fullData$steps <- mapply(missingValue, fullData$steps, fullData$interval)
 totalSteps <- tapply(fullData$steps, fullData$date, FUN = sum)
 qplot(totalSteps, binwidth = 1000, xlab = "Total number of steps taken each day")
 
-dev.copy(png, "figure/AverageStepsTakenAfterFillingMissingData.png")
+dev.copy(png, "figure/TotalStepsTakenAfterFillingMissingData.png")
 dev.off()
 
 mean(totalSteps)
 median(totalSteps)
 
+# Step 5
+# activity pattern between week days and week ends
+
+# 1. Create a new factor variable in the dataset with two levels – “weekday” and “weekend” indicating whether a given date is a weekday or weekend day.
+checkWeekDayOrWeekEnd <- function(date) {
+    day <- weekdays(date)
+    if (day %in% c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"))
+        return("weekday")
+    else 
+        return("weekend")
+}
+
+fullData$date <- as.Date(fullData$date)
+fullData$day <- sapply(fullData$date, FUN = checkWeekDayOrWeekEnd)
+
+#2. Make the plot now
+averageActivity <- aggregate(steps ~ interval + day, data = fullData, mean)
+ggplot(averageActivity, aes(interval, steps)) +
+    geom_line() +
+    facet_grid(day ~ .) +
+    xlab("Interval") +
+    ylab("Number of steps taken")
+
+dev.copy(png, "figure/ActivityPatternWeekDaysAndWeekEnds.png")
+dev.off()
